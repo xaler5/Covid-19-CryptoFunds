3d52090af521c8aaf05c609a1cebef09c0aa0842
Dario <dariolobuglio@gmail.com>
2020-03-21 23:37:12 +0100

    fix, deposits, withdrawals for ERC20 not working yet

--- contracts/Covid19CryptoFund.sol
+++ contracts/Covid19CryptoFund.sol
@@ -1,36 +1,56 @@
 pragma solidity <0.7.0;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.0.0/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.0.0/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.0.0/contracts/access/Ownable.sol";
 
 contract Covid19CryptoFund is Ownable {
+    
+    event EthReceived(uint256);
+    event ERC20Received(address, uint256);
+    
     using SafeMath for uint256;
 
     uint256 public weiBalance;
     mapping ( address => uint256 ) public balances;
 
-    function sendEth(address to, uint amount) external onlyOwner {
-        weiBalance.sub(amount);
+    function sendEth(address payable to, uint amount) external onlyOwner {
+        require(amount <= weiBalance, "Insufficient ethers balance");
+        
+        weiBalance = weiBalance.sub(amount);
 
         to.transfer(amount);
     }
 
     function sendERC20(address to, address erc20Contract, uint tokens) external onlyOwner {
-        balances[erc20Contract].sub(tokens);
+        require(tokens <= balances[erc20Contract], "Insufficient token balance");
         
-        IERC20(erc20Contract).transfer(msg.sender, balances[msg.sender]);
-    }
-
-    function receiveERC20(uint256 tokens, address erc20Contract) external {
-        IERC20(erc20Contract).approve(address(this), tokens);
-
-        IERC20(erc20Contract).transferFrom(msg.sender, address(this), tokens);
-
-        balances[erc20Contract].add(tokens);
+        balances[erc20Contract] = balances[erc20Contract].sub(tokens);
+        
+        bool result = IERC20(erc20Contract).transfer(to, tokens);
+        
+        require(result, "Something went wrong");
     }
     
-    receive() external payable {
-        weiBalance.add(msg.value);
+    function receiveERC20(uint256 tokens, address erc20Contract) external {
+        erc20Contract.delegatecall(
+            abi.encodeWithSignature(
+                "transfer(address,uint)",
+                address(this),
+                tokens
+            )
+        );
+
+        balances[erc20Contract] = balances[erc20Contract].add(tokens);
+
+        emit ERC20Received(erc20Contract, tokens);
+    }
+
+    function receiveEth() external payable {
+        require(msg.value > 0, "No ethers sent");
+        
+        emit EthReceived(msg.value);
+        
+        weiBalance = weiBalance.add(msg.value);
     }
 }
